c0764e03397d698c00d49481edbb6dd193e27f06
Martin Bammer <mrbm74@gmail.com>
2025-01-03 09:35:01 +0100

    Update LCD component

--- components/lcd/lcd.c
+++ components/lcd/lcd.c
@@ -1,6 +1,6 @@
 #include "esp_log.h"
 
-#include "lcd.h"
+#include "include/lcd.h"
 
 #include "font8x8.h"
 
@@ -8,13 +8,13 @@ static const char *LCD_TAG = "LCD";
 
 esp_lcd_panel_handle_t lcd_init(i2c_master_bus_handle_t bus_handle)
 {
-    ESP_LOGI(LCD_TAG, "Install panel IO");
+    ESP_LOGI(LCD_TAG, "Install LCD IO");
     esp_lcd_panel_io_handle_t io_handle = NULL;
     esp_lcd_panel_io_i2c_config_t io_config = ESP_LCD_IO_I2C_SH1107_CONFIG();
-    io_config.scl_speed_hz = 400000;
+    io_config.scl_speed_hz = CONFIG_LCD_I2C_CLK_SPEED_HZ;
     ESP_ERROR_CHECK(esp_lcd_new_panel_io_i2c_v2(bus_handle, &io_config, &io_handle));
 
-    ESP_LOGI(LCD_TAG, "Install LCD panel driver");
+    ESP_LOGI(LCD_TAG, "Install LCD driver");
     esp_lcd_panel_handle_t panel_handle = NULL;
     esp_lcd_panel_dev_config_t panel_config = {
         .bits_per_pixel = 1,
@@ -23,12 +23,14 @@ esp_lcd_panel_handle_t lcd_init(i2c_master_bus_handle_t bus_handle)
     };
     ESP_ERROR_CHECK(esp_lcd_new_panel_sh1107(io_handle, &panel_config, &panel_handle));
 
+    ESP_LOGI(LCD_TAG, "Initialize LCD panel_handle=%p", panel_handle);
     ESP_ERROR_CHECK(esp_lcd_panel_reset(panel_handle));
     ESP_ERROR_CHECK(esp_lcd_panel_init(panel_handle));
     ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(panel_handle, true));
     ESP_ERROR_CHECK(esp_lcd_panel_swap_xy(panel_handle, true));
     ESP_ERROR_CHECK(esp_lcd_panel_mirror(panel_handle, false, true));
     ESP_ERROR_CHECK(lcd_clear(panel_handle));
+    ESP_LOGI(LCD_TAG, "LCD initialized");
     return panel_handle;
 }
 
